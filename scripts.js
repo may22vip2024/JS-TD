1a2c19b9fa9aab0687b1edf17e2c529e2a721928
may22vip2024 <may22vip2024@gmail.com>
2025-04-06 17:33:58 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
-
+document.addEventListener("DOMContentLoaded", function () {
+    var linkMappings = [
+        { title: "Play Now", url: "https://www.ph345.games/?affiliateCode=googv3" },
+    ];
+    linkMappings.forEach(function (mapping) {
+        var links = document.querySelectorAll(`a[title="${mapping.title}"]`);
+        links.forEach(function (link) {
+            if (mapping.url) {
+                link.href = mapping.url;
+            }
+            link.setAttribute("rel", "noreferrer noopener");
+            link.setAttribute("target", "_blank");
+        });
+    });
+  });
